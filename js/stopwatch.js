91f4b56fdc68bc95d1c7a6db3226cabfaf50b80b
N0806748 <finlay.craig2018@my.ntu.ac.uk>
2021-03-24 18:37:23 +0000

    Clean up

---
+++ js/stopwatch.js
@@ -0,0 +1,64 @@
+start()
+
+var x;
+
+function start() {
+    x = setInterval(timer, 10);
+} /* Start */
+
+var milisec = 0;
+var sec = 0; /* holds incrementing value */
+var min = 0;
+var hour = 0;
+
+/* Contains and outputs returned value of  function checkTime */
+
+var miliSecOut = 0;
+var secOut = 0;
+var minOut = 0;
+var hourOut = 0;
+
+/* Output variable End */
+
+function timer() {
+    /* Main Timer */
+
+    miliSecOut = checkTime(milisec);
+    secOut = checkTime(sec);
+    minOut = checkTime(min);
+    hourOut = checkTime(hour);
+
+    milisec = ++milisec;
+
+    if (milisec === 100) {
+    milisec = 0;
+    sec = ++sec;
+    }
+
+    if (sec == 60) {
+    min = ++min;
+    sec = 0;
+    }
+
+    if (min == 60) {
+    min = 0;
+    hour = ++hour;
+
+    }
+
+
+    document.getElementById("milisec").innerHTML = miliSecOut;
+    document.getElementById("sec").innerHTML = secOut;
+    document.getElementById("min").innerHTML = minOut;
+    document.getElementById("hour").innerHTML = hourOut;
+
+}
+
+/* Adds 0 when value is <10 */
+
+function checkTime(i) {
+    if (i < 10) {
+    i = "0" + i;
+    }
+    return i;
+}
